3648e2b4900c60ce277e53ae341e4a922732444b
cherrypicked12 <zhouzhoujiang12@gmail.com>
2023-10-25 20:58:11 -0400

    hi

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ def encode(password):
     encoded_pass = ""
     for num in range(0, len(password)):
         encoded_num = str(int(password[num]) + 3)
+        if len(encoded_num) == 2:
+            encoded_num = encoded_num[1:]
         encoded_pass += encoded_num
     return encoded_pass
